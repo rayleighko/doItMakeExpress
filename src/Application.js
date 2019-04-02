129131e5718470ec0e79128b88c44708d8a6b7c8
ray <gmj1197@gmail.com>
2019-04-02 15:07:55 +0900

    정적파일 다루는 로직

--- src/Application.js
+++ src/Application.js
@@ -1,12 +1,41 @@
-const http = require('http')
-const debug = require('../utils/debug')('Application')
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
+const debug = require('../utils/debug')('Application');
+
+const mimeType = {
+	'.ico': 'image/x-icon',
+	'.html': 'text/html',
+	'.js': 'text/javascript',
+	'.css': 'text/css',
+	'.png': 'image/png',
+	'.jpg': 'image/jpeg',
+	'.eot': 'appliaction/vnd.ms-fontobject',
+	'.ttf': 'aplication/font-sfnt'
+}
 
 const Application = () => {
 	const _server = http.createServer((req, res) => {
-		res.statusCode = 200;
-		res.setHeader('Content-Type', 'text/plain');
-		res.end('Hello World\n');
+
+		const ext = path.parse(req.url).ext;
+		const publicPath = path.join(__dirname, '../public')
+
+		if (Object.keys(mimeType).includes(ext)) {
+			fs.readFile(`${publicPath}${req.url}`, (err, data) => {
+				if (err) {
+					res.statusCode = 404;
+					res.end('Not found');
+				} else {
+					res.statusCode = 200
+					res.setHeader('Content-Type', mimeType[ext]);
+					res.end(data)
+				}
+			})
+		} else {
+			res.statusCode = 200;
+		}
 	});
+
 
 	const listen = (port = 3000, hostname = '127.0.0.1', fn) => {
 		_server.listen(port, hostname, fn)
@@ -17,6 +46,7 @@ const Application = () => {
 		_server,
 		listen
 	}
+
 }
 
 module.exports = Application;
