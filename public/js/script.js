ca4312dc060c77afe6b1c1a9a5204cc8eac254b6
Rayleigh <gmj1197@gmail.com>
2019-04-04 21:29:14 +0900

    post 메서드 추가

--- public/js/script.js
+++ public/js/script.js
@@ -1,57 +1,99 @@
 (() => {
-	const api = {
-		getPosts() {
-			return http('get', '/api/posts')
-		}
-	}
+  const http = (method, url, data = null) =>
+    new Promise((resolve, reject) => {
+      const req = new XMLHttpRequest();
+      req.open(method, url, true);
+      req.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
 
-	const http = (method, url, data = null) => new Promise((resolve, reject) => {
-		const req = new  XMLHttpRequest()
-		req.open(method, url, true)
-		req.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-		req.onreadystatechange = evt => {
-			if (req.readyState == 4) {
-				let responseText = req.responseText.trim()
-				responseText = responseText ? JSON.parse(responseText) : {}
+      req.onreadystatechange = evt => {
+        if (req.readyState == 4) {
+          let responseText = req.responseText.trim();
+          responseText = responseText ? JSON.parse(responseText) : {};
 
-				if(req.status >= 200 && req.status < 300) {
-					resolve(responseText)
-				} else {
-					reject({
-						status: req.status,
-						responseText
-					})
-				}
-			}
-		}
-		req.send(data)
-	})
+          if (req.status >= 200 && req.status < 300) {
+            resolve(responseText);
+          } else {
+            reject({
+              status: req.status,
+              responseText
+            });
+          }
+        }
+      };
+      req.send(data);
+    });
 
-	const loadTimeline = el => {
-		el.innerHTML = 'Loading...'
+  const api = {
+    getPosts() {
+      return http("get", "/api/posts?limit=5&page=1");
+    },
+    createPost({ title, body }) {
+      const data = `title=${title}&body=${body}`;
+      return http("post", "/api/posts", data);
+    }
+  };
 
-		api.getPosts()
-			.then(data => {
-				el.innerHTML = data.reduce((html, post) => {
-					html += `
-            <h2>${post.title}</h2>
-            <div>${post.body}</div>
-          `
-					return html
-				}, '')
-			})
-			.catch(err => {
-				console.log(err)
-				el.innerHTML = 'Error: Refresh this page'
-			})
-	}
+  const loadTimeline = el => {
+    el.innerHTML = "Loading...";
 
-	const onload = () => {
-		const timelineEl = document.querySelector('#timeline')
-		if (!timelineEl) throw Error ('#timeline element is required')
-		loadTimeline(timelineEl)
+    api
+      .getPosts()
+      .then(data => {
+        el.innerHTML = data.reduce((html, post) => {
+          html += `
+			  <h2>${post.title}</h2>
+			  <div>${post.body}</div>
+			`;
+          return html;
+        }, "");
+      })
+      .catch(err => {
+        console.log(err);
+        el.innerHTML = "Error: Refresh this page";
+      });
+  };
 
-	}
+  const initAddPostForm = el => {
+    const onSubmitAddPostForm = e => {
+      e.preventDefault();
 
-	document.addEventListener('DOMContentLoaded', onload);
-})()
+      const formMsgEl = document.querySelector("#form-msg");
+      formMsgEl.innerHTML = "";
+
+      let { title = {}, body = {} } = e.target;
+      title = title.value || "";
+      body = body.value || "";
+
+      if (!title || !body) {
+        return (formMsgEl.innerHTML = "Insert text and body");
+      }
+
+      api
+        .createPost({ title, body })
+        .then(data => {
+          formMsgEl.innerHTML = "";
+          e.target.reset();
+
+          const timelineEl = document.querySelector("#timeline");
+          if (!timelineEl) throw Error("#timeline element is required");
+          loadTimeline(timelineEl);
+        })
+        .catch(err => {
+          formMsgEl.innerHTML = "Error: Retry";
+        });
+    };
+    el.addEventListener("submit", onSubmitAddPostForm);
+  };
+
+  const onload = () => {
+    const timelineEl = document.querySelector("#timeline");
+    if (!timelineEl) throw Error("#timeline element is required");
+    loadTimeline(timelineEl);
+
+    const formEl = document.querySelector("#add-post-form");
+    if (!formEl) throw Error("#add-post-form element is required");
+    initAddPostForm(formEl);
+  };
+
+  document.addEventListener("DOMContentLoaded", onload);
+})();
--- routes/api/post.js
+++ routes/api/post.js
@@ -1,4 +1,4 @@
-const posts = [
+let posts = [
   { title: "post 3", body: "this is post 3" },
   { title: "post 2", body: "this is post 2" },
   { title: "post 1", body: "this is post 1" }
@@ -15,7 +15,15 @@ const index = () => (req, res, next) => {
 };
 
 const create = () => (req, res, next) => {
-  debug(`create() ${req.body}`);
+  const { title, body } = req.body;
+  const post = { title, body };
+
+  if (!post.title || !post.body) {
+    return res.status(400).send("parameter error");
+  }
+  posts = [post].concat(posts);
+
+  res.status(201).json(post);
 };
 
 module.exports = {
