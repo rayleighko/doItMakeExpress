5a316a62e34c57fe7767e60cfd54901bacacbb61
Rayleigh <gmj1197@gmail.com>
2019-04-04 21:27:21 +0900

    쿼리의 바디 데이터 처리를 위한 bodyParser 미들웨어 추가

--- app.js
+++ app.js
@@ -4,11 +4,13 @@ const logger = require("./middlewares/logger");
 const errors = require("./middlewares/errors");
 const index = require("./routes/index");
 const apiPost = require("./routes/api/post");
+const bodyParser = require("./middlewares/bodyParser");
 const App = require("./src/Application");
 const app = App();
 
 app.use(logger());
 app.use(serveStatic());
+app.use(bodyParser());
 app.use("/", index.listPosts());
 app.get("/api/posts", apiPost.index());
 app.post("/api/posts", apiPost.create());
